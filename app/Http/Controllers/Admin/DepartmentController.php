40e4e931251faf2bc27e21dbf1f9cd95a2b7808b
sahab uddin <sahabuddinriyaj984@gmail.com>
2019-09-13 16:25:04 +0600

    modifi departmentcontroller

--- app/Http/Controllers/Admin/DepartmentController.php
+++ app/Http/Controllers/Admin/DepartmentController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
 
-class DepartmentController extends Controller
+class DepartmentController extends BaseController
 {
     //
 }
